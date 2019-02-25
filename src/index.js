52dea791e9e9fb20518cfc907b0652fa6a6c1162
olympiawojcik <owojcik7@gmail.com>
2019-02-25 16:08:42 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
